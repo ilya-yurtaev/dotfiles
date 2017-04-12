61a49ff3dac2036a4fd2b6b9fafc3d39c777f42f
ilya.yurtaev@gmail.com <ilya.yurtaev@gmail.com>
2017-04-12 17:00:46 +0300

    premerge

--- files/.emacs.d/init.el
+++ files/.emacs.d/init.el
@@ -147,6 +147,7 @@
 
 ;; flycheck
 (add-hook 'after-init-hook #'global-flycheck-mode)
+(setq flycheck-highlighting-mode 'line)
 
 ;; neotree
 (require 'neotree)
@@ -255,9 +256,9 @@
               (append flycheck-disabled-checkers
                       '(json-jsonlist)))
 
-(setq web-mode-markup-indent-offset 4)
-(setq web-mode-css-indent-offset 4)
-(setq web-mode-code-indent-offset 4)
+(setq web-mode-markup-indent-offset 2)
+(setq web-mode-css-indent-offset 2)
+(setq web-mode-code-indent-offset 2)
 (setq web-mode-engines-alist
       '(("django" . "\\.html\\'")))
 
